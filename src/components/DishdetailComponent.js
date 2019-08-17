401e7da0c784b8747040b1d91ea8600a40a58c65
Dinesh <dineshbode@gmail.com>
2019-08-17 13:02:55 +0530

    Week1 course excercise

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Dishdetail extends Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return{
+            
+        }
+    }
+}
